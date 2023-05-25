24a0a3c4099a78c3d5f1479ce6ccfcdf8e26fac9
EG-AKANDE <emmanuel_akande@yahoo.com>
2023-05-24 21:06:13 -0700

    fixed strcmp

--- _strcmp.c
+++ _strcmp.c
@@ -4,10 +4,8 @@
  * _strcmp - compares two strings
  * @str1: first string(left string)
  * @str2: second string(right string)
- * Return: if str2 is not null, it indicates that
- *         str1 is greater and returns the difference
- *         the two strings
- *         else both strings are equal and 0 is returned
+ *
+ * Return: difference if nut null, else 0
  */
 
 int _strcmp(char *str1, char *str2)
